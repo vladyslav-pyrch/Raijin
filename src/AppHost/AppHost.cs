064baddda19c5912210190670e30759baede4c97
Vladyslav Pyrch <vlad.nuron@gmail.com>
2025-06-26 19:07:05 +0200

    Implemented: tests run before services

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -2,10 +2,20 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var tests = builder.AddExecutable(
+    "tests",
+    "dotnet",
+    "../../",
+    "test"
+
+);
+
 var problemSolvingServiceApi = builder.AddProject<Njinx_ProblemSolvingService_Api>(
-    "problem-solving-service-api"
-).WithHttpHealthCheck("/health")
-.WithExternalHttpEndpoints();
+        "problem-solving-service-api"
+    ).WithHttpHealthCheck("/health")
+    .WithExternalHttpEndpoints()
+    .WaitForCompletion(tests);
+
 
 builder.AddNpmApp("angular-spa", "../Spa")
     .WithReference(problemSolvingServiceApi)
