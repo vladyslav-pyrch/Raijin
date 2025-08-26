396dc723585f5aa2139f40f488c915bbdb19f956
Vladyslav Pyrch <pyrch.vladyslav@gmail.com>
2025-08-26 22:34:26 +0200

    RAI-27 make extension method for IBooleanExpression

---
+++ src/ProblemSolvingService/Domain/BooleanFormulas/BooleanExpressionExtensions.cs
@@ -0,0 +1,21 @@
+namespace Raijin.ProblemSolvingService.Domain.BooleanFormulas;
+
+public static class BooleanExpressionExtensions
+{
+    public static Negation Negated(this IBooleanExpression expression) => new(expression);
+
+    public static Implication Imply(this IBooleanExpression expression1, IBooleanExpression expression2) =>
+        new(Condition: expression1, Consequence: expression2);
+
+    public static ExclusiveDisjunction Xor(this IBooleanExpression expression1, IBooleanExpression expression2) =>
+        new(expression1, expression2);
+
+    public static Disjunction Or(this IBooleanExpression expression1, IBooleanExpression expression2) =>
+        new(expression1, expression2);
+
+    public static Conjunction And(this IBooleanExpression expression1, IBooleanExpression expression2) =>
+        new(expression1, expression2);
+
+    public static Equivalence Equal(this IBooleanExpression expression1, IBooleanExpression expression2) =>
+        new(expression1, expression2);
+}
---
+++ tests/ProblemSolvingService/Domain.Tests/BooleanFormulas/BooleanExpressionExtensionsTests.cs
@@ -0,0 +1,74 @@
+using FluentAssertions;
+using NSubstitute;
+using Raijin.ProblemSolvingService.Domain.BooleanFormulas;
+
+namespace Raijin.ProblemSolvingService.Domain.Tests.BooleanFormulas;
+
+public class BooleanExpressionExtensionsTests
+{
+    [Fact]
+    public void GivenBooleanExpression_WhenNegated_ReturnsNegationOfBooleanExpression()
+    {
+        var booleanExpression = Substitute.For<IBooleanExpression>();
+
+        Negation negatedBooleanExpression = booleanExpression.Negated();
+
+        negatedBooleanExpression.Should().BeEquivalentTo(new Negation(booleanExpression));
+    }
+
+    [Fact]
+    public void GivenTwoBooleanExpression_WhenImplying_ReturnsImplication()
+    {
+        var booleanExpression1 = Substitute.For<IBooleanExpression>();
+        var booleanExpression2 = Substitute.For<IBooleanExpression>();
+
+        Implication implication = booleanExpression1.Imply(booleanExpression2);
+
+        implication.Should()
+            .BeEquivalentTo(new Implication(Condition: booleanExpression1, Consequence: booleanExpression2));
+    }
+
+    [Fact]
+    public void GivenTwoBooleanExpression_WhenAnd_ReturnsConjunction()
+    {
+        var booleanExpression1 = Substitute.For<IBooleanExpression>();
+        var booleanExpression2 = Substitute.For<IBooleanExpression>();
+
+        Conjunction conjunction = booleanExpression1.And(booleanExpression2);
+
+        conjunction.Should().BeEquivalentTo(new Conjunction(booleanExpression1, booleanExpression2));
+    }
+
+    [Fact]
+    public void GivenTwoBooleanExpression_WhenOr_ReturnsDisjunction()
+    {
+        var booleanExpression1 = Substitute.For<IBooleanExpression>();
+        var booleanExpression2 = Substitute.For<IBooleanExpression>();
+
+        Disjunction disjunction = booleanExpression1.Or(booleanExpression2);
+
+        disjunction.Should().BeEquivalentTo(new Disjunction(booleanExpression1, booleanExpression2));
+    }
+
+    [Fact]
+    public void GivenTwoBooleanExpression_WhenXor_ReturnsExclusiveDisjunction()
+    {
+        var booleanExpression1 = Substitute.For<IBooleanExpression>();
+        var booleanExpression2 = Substitute.For<IBooleanExpression>();
+
+        ExclusiveDisjunction exclusiveDisjunction = booleanExpression1.Xor(booleanExpression2);
+
+        exclusiveDisjunction.Should().BeEquivalentTo(new ExclusiveDisjunction(booleanExpression1, booleanExpression2));
+    }
+
+    [Fact]
+    public void GivenTwoBooleanExpression_WhenEqualing_ReturnsEquivalence()
+    {
+        var booleanExpression1 = Substitute.For<IBooleanExpression>();
+        var booleanExpression2 = Substitute.For<IBooleanExpression>();
+
+        Equivalence equivalence = booleanExpression1.Equal(booleanExpression2);
+
+        equivalence.Should().BeEquivalentTo(new Equivalence(booleanExpression1, booleanExpression2));
+    }
+}
