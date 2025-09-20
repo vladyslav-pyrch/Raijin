5809abeb07b7125864cf5e5da695f7c3c067c2ce
Vladyslav Pyrch <pyrch.vladyslav@gmail.com>
2025-09-20 21:40:56 +0200

    RAI-42 Added swagger, updated dependencies, cleaned up DI

--- src/ProblemSolvingService/Infrastructure/InfrastructureModule.cs
+++ src/ProblemSolvingService/Infrastructure/InfrastructureModule.cs
@@ -1,3 +1,55 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Raijin.Constants;
+using Raijin.ProblemSolvingService.Application.Cqrs;
+using Raijin.ProblemSolvingService.Application.Features;
+using Raijin.ProblemSolvingService.Infrastructure.Cqrs;
+using Raijin.ProblemSolvingService.Infrastructure.CryptoMiniSat;
+
 namespace Raijin.ProblemSolvingService.Infrastructure;
 
-public static class InfrastructureModule;
+public static class InfrastructureModule
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddCqrs();
+        services.AddCryptoMiniSat();
+
+        return services;
+    }
+
+    private static IServiceCollection AddCqrs(this IServiceCollection services) =>
+        services.AddScoped<ISender, DotNetDiSender>();
+
+    private static IServiceCollection AddCryptoMiniSat(this IServiceCollection services)
+    {
+        services.AddScoped<ISatSolver, CryptominisatSatSolver>();
+        services.AddScoped<Cryptominisat>();
+        services.AddOptions<CryptominisatOptions>()
+            .Configure((CryptominisatOptions options, IConfiguration configuration) =>
+            {
+                options.ContainerName = configuration[EnvironmentVariables.Cryptominisat.ContainerName]!;
+                options.FileExchangeContainerPath =
+                    configuration[EnvironmentVariables.Cryptominisat.FileExchangeContainerPath]!;
+                options.FileExchangeLocalPath =
+                    configuration[EnvironmentVariables.Cryptominisat.FileExchangeLocalPath]!;
+                options.TimeoutSeconds =
+                    Convert.ToInt32(configuration[EnvironmentVariables.Cryptominisat.TimeoutSeconds]);
+            })
+            .Validate(
+                options => !string.IsNullOrWhiteSpace(options.ContainerName),
+                "Cryptominisat container name is required."
+            ).Validate(
+                options => !string.IsNullOrWhiteSpace(options.FileExchangeContainerPath),
+                "Cryptominisat container file exchange path is required."
+            ).Validate(
+                options => !string.IsNullOrWhiteSpace(options.FileExchangeLocalPath),
+                "Cryptominisat local file exchange path is required."
+            ).Validate(
+                options => options.TimeoutSeconds >= 0,
+                "Cryptominisat timeout may not be negative."
+            );
+
+        return services;
+    }
+}
