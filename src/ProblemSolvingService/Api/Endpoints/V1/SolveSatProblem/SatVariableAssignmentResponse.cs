74141e3391fdfedbb5d76d6bd4e66c953fefc5b0
Vladyslav Pyrch <pyrch.vladyslav@gmail.com>
2026-01-11 17:17:58 +0100

    all done

--- src/ProblemSolvingService/Api/Endpoints/V1/SolveSatProblem/SatVariableAssignmentResponse.cs
+++
@@ -1,8 +0,0 @@
-using Raijin.ProblemSolvingService.Application.Features.Dtos;
-
-namespace Raijin.ProblemSolvingService.Api.Endpoints.V1.SolveSatProblem;
-
-public sealed record SatVariableAssignmentResponse(int VariableNumber, bool Assignment)
-{
-    public static SatVariableAssignmentResponse From(SatVariableAssignmentDto dto) => new(dto.VariableNumber, dto.Assignment);
-}
--- src/ProblemSolvingService/Application/Features/SolveSatProblem/SolveSatProblemCommand.cs
+++
@@ -1,6 +0,0 @@
-using Raijin.ProblemSolvingService.Application.Cqrs;
-using Raijin.ProblemSolvingService.Application.Features.Dtos;
-
-namespace Raijin.ProblemSolvingService.Application.Features.SolveSatProblem;
-
-public sealed record SolveSatProblemCommand(List<ClauseDto> Clauses) : IRequest<SolveSatProblemResult>;
