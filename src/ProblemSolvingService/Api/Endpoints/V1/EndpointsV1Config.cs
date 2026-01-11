20bac5e6e3a53a0adc9f79f81c484c33792685c6
Vladyslav Pyrch <pyrch.vladyslav@gmail.com>
2026-01-11 17:20:10 +0100

    all done (#5)

--- src/ProblemSolvingService/Api/Endpoints/V1/EndpointsV1Config.cs
+++ src/ProblemSolvingService/Api/Endpoints/V1/EndpointsV1Config.cs
@@ -1,6 +1,5 @@
 using Raijin.ProblemSolvingService.Api.Endpoints.V1.SolveBooleanExpression;
 using Raijin.ProblemSolvingService.Api.Endpoints.V1.SolveSatExpression;
-using Raijin.ProblemSolvingService.Api.Endpoints.V1.SolveSatProblem;
 
 namespace Raijin.ProblemSolvingService.Api.Endpoints.V1;
 
@@ -8,7 +7,6 @@ public static class EndpointsV1Config
 {
     public static IEndpointRouteBuilder MapEndpoints(this IEndpointRouteBuilder app)
     {
-        app.MapPost("/v1/solve-sat", SolveSatProblemEndpoint.Execute);
         app.MapPost("/v1/solve-sat-expression", SolveSatExpressionEndpoint.Execute);
         app.MapPost("/v1/solve-boolean-expression", SolveBooleanExpressionEndpoint.Execute);
 
