d310961a6161f8630810488a3ed08470875893b0
Vladyslav Pyrch <pyrch.vladyslav@gmail.com>
2026-05-01 02:07:13 +0200

    feat: remove Scheduling folder from Problems feature and API projects

--- src/CombinatoricsService/Domain/Problems/Scheduling/Resource.cs
+++
@@ -1,6 +0,0 @@
-namespace Raijin.CombinatoricsService.Domain.Problems.Scheduling;
-
-/// <summary>
-///     A resource that can be assigned to tasks. Tasks requiring the same resource cannot overlap.
-/// </summary>
-public sealed record Resource(string Id);
